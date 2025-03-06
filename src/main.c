2314794108d8e6373e17ae067655a419fbfef0d3
Zuma <zuma@zuma.eu.org>
2025-03-06 20:04:31 +0000

    Changed how SDL3 is included

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
+#include "SDL3/SDL_init.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  printf("Hello, World!\n");
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    fprintf(stderr, "Failed to init SDL\n");
+  }
+
+  SDL_Window *window = SDL_CreateWindow("js2d", 800, 600, 0);
+  if (window == NULL) {
+    fprintf(stderr, "Failed to create a window\n");
+  }
+
+  while (true) {
+  }
+
   return EXIT_SUCCESS;
 }
